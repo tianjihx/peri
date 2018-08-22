87e5f0e9a800ffcf70bb7205444271da197d0ee2
tianjihx <tianjihx@126.com>
2018-08-22 17:54:16 +0800

    首次提交

---
+++ Assets/Editor/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Peri
+{
+    public class Button
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+        public string Text { get; set; }
+
+        public delegate void OnClickDelegate();
+        public event OnClickDelegate OnClick;
+
+        public void Draw()
+        {
+            GUIStyle style = new GUIStyle(EditorStyles.miniButton);
+            if (GUI.Button(new Rect(Position, Size), Text))
+            {
+                OnClick?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Editor/Peri.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Peri
+{
+    public class UICreator : EditorWindow
+    {
+        public Button button;
+
+        [MenuItem("Window/窗口编辑器")]
+        static void DoIt()
+        {
+            Rect wr = new Rect(0, 0, 500, 500);
+            var window = GetWindowWithRect<UICreator>(wr, true, "窗口编辑器");
+            window.Show();
+        }
+
+        public UICreator()
+        {
+            ComponentInitialization();
+        }
+
+        private void ComponentInitialization()
+        {
+            button = new Button();
+            button.Position = new Vector2(100, 100);
+            button.Size = new Vector2(50, 50);
+            button.Text = "click me";
+            button.OnClick += () => { EditorUtility.DisplayDialog("adsf", "ad", "123"); };
+        }
+
+        private void OnGUI()
+        {
+            button.Draw();
+        }
+    }
+}
+
